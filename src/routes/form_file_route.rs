aac6246b5f55e88bc4bd85281623bed56766a2a0
desonglll <lindesong666@163.com>
2024-06-02 15:02:30 +0800

    change single upload to mutiple upload for form upload.

--- src/routes/form_file_route.rs
+++ src/routes/form_file_route.rs
@@ -1,35 +1,47 @@
-use std::path::Path;
-
 use rocket::form::{Form, FromForm};
 use rocket::fs::TempFile;
+use rocket::serde::json::Json;
+use rocket::serde::Serialize;
 use rocket::{get, post};
-
+use std::path::Path;
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ResponseData {
+    paths: Vec<String>,
+}
 #[derive(FromForm)]
 pub struct Upload<'r> {
     save: bool,
-    file: TempFile<'r>,
+    file: Vec<TempFile<'r>>,
 }
 #[post("/upload", data = "<upload>")]
-pub async fn upload(upload: Form<Upload<'_>>) -> std::io::Result<String> {
+pub async fn upload(upload: Form<Upload<'_>>) -> Result<Json<ResponseData>, std::io::Error> {
     println!("{:?}", upload.file);
     let upload_data = upload.into_inner();
     if upload_data.save {
         // 如果 save 字段为 true，保存文件
         // 在这里处理上传的文件，例如将其保存到磁盘或进行其他操作
         // 例如：
-        let mut file = upload_data.file;
-        let root = concat!(env!("CARGO_MANIFEST_DIR"), "/", "upload");
-        println!("root: {:?}", root);
-        let file_name = file.raw_name().unwrap().dangerous_unsafe_unsanitized_raw().to_string();
-        println!("filename: {:?}", file_name);
-        let file_path = Path::new(root).join(file_name.clone());
-        println!("file_path: {:?}", file_path.clone().to_str());
 
-        file.persist_to(file_path.clone()).await?;
-
-        Ok(String::from(file_name))
+        let file = upload_data.file;
+        let mut paths = Vec::new();
+        for mut f in file {
+            let root = concat!(env!("CARGO_MANIFEST_DIR"), "/", "upload");
+            println!("root: {:?}", root);
+            let file_name = f.raw_name().unwrap().dangerous_unsafe_unsanitized_raw().to_string();
+            println!("filename: {:?}", file_name);
+            let file_path = Path::new(root).join(file_name.clone());
+            println!("file_path: {:?}", file_path.clone().to_str());
+            f.persist_to(file_path.clone()).await?;
+            paths.push(String::from(file_name))
+        }
+        Ok(Json(ResponseData {
+            paths,
+        }))
     } else {
-        Ok(String::new())
+        Ok(Json(ResponseData {
+            paths: Vec::new(),
+        }))
     }
 }
 #[get("/retrieve/<path>")]
