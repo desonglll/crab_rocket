37097ff75556e43adcc6fb7112075f632f0af8ef
desonglll <lindesong666@163.com>
2024-06-17 14:36:05 +0800

    update ui2.0

--- ui/v2.0/src/router/CommonRoutes.tsx
+++ ui/v2.0/src/router/CommonRoutes.tsx
@@ -1,7 +1,7 @@
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Dashboard} from "../pages/Dashboard.tsx";
 
-export const Routes = () => {
+export const CommonRoutes = () => {
     return (
         <>
             <BrowserRouter>
