6763d8d685a84d509f5a4648d752c438f4143dfb
mikeshinoda <53910940+desonglll@users.noreply.github.com>
2024-08-01 12:31:59 +0800

    Add Material UI.

---
+++ ui/v0.2/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;
